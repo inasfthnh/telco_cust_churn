8a731ea71e7c8abe8f1be034151ad9d2646f469a
Inas Fathinah S <61815171+inasfthnh@users.noreply.github.com>
2024-05-30 15:22:08 +0700

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -1,7 +1,7 @@
 # tempat pemrosesan machine learning ke app.py
 import streamlit as st
 import numpy as np
-from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import LabelEncoder, RobustScaler
 import pandas as pd
 from datetime import date, datetime
 
@@ -11,18 +11,26 @@ import os
 
 attribute_info = """
                 Penjelasan untuk tiap-tiap kolom :
-                - ID SPK : ID Transaksi, Nomor identifikasi unik untuk setiap transaksi yang masuk.
-                - Tipe Order : Menunjukkan jenis pesanan yang dilakukan oleh pelanggan.
-                    - Order : Transaksi pertama dari pelanggan.
-                    - Re-Order : Transaksi kedua dari pelanggan untuk judul yang berbeda.
-                    - Cetak Ulang : Transaksi kedua dari pelanggan untuk judul yang sama.
-                    - Retur: Transaksi di mana nilai transaksi menjadi 0 karena pelanggan kecewa sehingga buku dikembalikan kepada penerbit. Jika pelanggan mengembalikan barang dan menginginkan barang baru karena kesalahan internal, transaksi akan diganti dengan nilai 100%.
-                - Tanggal Masuk Naskah : Tanggal ketika naskah pertama kali diterima atau dimasukkan ke dalam sistem.
-                - Tanggal Mulai Proses : Tanggal ketika proses pra-cetak dimulai untuk naskah yang diterima.
-                - Judul Naskah : Judul naskah buku
-                - Tanggal Deal : Tanggal ketika penulis menyetujui hasil layout buku.
-                - Sales Order: Nominal yang dibayar oleh pelanggan untuk transaksi tersebut.
-                - Kuantitas: Jumlah eksemplar buku yang dicetak dalam transaksi tersebut.
+                - customerID : ID yang bernilai unik untuk tiap customer
+                - gender : male atau female
+                - SeniorCitizen : No atau Yes
+                - Partner : jika customer memiliki partner atau tidak
+                - Dependents : jika customer memiliki tanggungan atau tidak
+                - PhoneService
+                - MultipleLines : 'No phone service', 'No', 'Yes'
+                - InternetService : 'DSL', 'Fiber optic', 'No'
+                - OnlineSecurity : 'No', 'Yes', 'No internet service'
+                - OnlineBackup : 'Yes', 'No', 'No internet service'
+                - DeviceProtection : 'No', 'Yes', 'No internet service'
+                - TechSupport : 'No', 'Yes', 'No internet service'
+                - StreamingTV : 'No', 'Yes', 'No internet service'
+                - StreamingMovies : 'No', 'Yes', 'No internet service'
+                - tenure : seberapa lama (bulan) menjadi customer
+                - Contract : jangka waktu kontrak bernilai 'Month-to-month', 'One year', atau 'Two year'
+                - PaperlessBilling : jika customer memilih billing dengan paperless atau tidak
+                - PaymentMethod : 'Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)
+                - MonthlyCharges : tagihan bulanan
+                - TotalCharges : total tagihan
                 """
 
 
@@ -38,89 +46,111 @@ def run_ml_app():
 
     st.subheader("Input Your Data")
     with st.form("my_data"):
-        id_spk = st.number_input("ID SPK", step=1)
-        tipe_order = st.selectbox("Tipe Order", [
-                                  'order', 'reorder', 'cetak ulang', 'order ebook', 'cancel', 'retur'])
-        id_cust = st.number_input("ID Konsumen", step=1)
-        tgl_naskah = st.date_input("Tgl Masuk Naskah", value=date.today())
-        tgl_proses = st.date_input("Tgl Mulai Proses", value=date.today(), help="Mohon input tanggal lebih dari atau sama dengan Tgl Masuk Naskah")
-        judul_naskah = st.text_input(
-            "Judul Naskah", value="", max_chars=None, type="default", placeholder=None)
-        tgl_deal = st.date_input("Tanggal Deal", value=date.today(), help="Mohon input tanggal lebih dari atau sama dengan Tgl Mulai Proses")
-        sales_order = st.number_input("Sales Order", step=1)
-        kuantitas = st.number_input("Kuantitas", step=1)
+        customerID = st.text_input("Customer ID", value="", max_chars=10, type="default", placeholder=None)
+        gender = st.selectbox("Gender", ['Male', 'Female'])
+        SeniorCitizen = st.selectbox("Senior Citizen", ['Yes', 'No'])
+        Partner = st.selectbox("Partner", ['Yes', 'No'])
+        Dependents = st.selectbox("Dependents", ['Yes', 'No'])
+        tenure = st.number_input("Tenure", step=1)
+        PhoneService = st.selectbox("Phone Service", ['Yes', 'No'])
+        if PhoneService == 'No':
+          MultipleLines = st.selectbox("Multiple Lines", options=['Yes', 'No'], disabled=True)
+          MultipleLines = 'No Phone Service'
+        else:
+          MultipleLines = st.selectbox("Multiple Lines", options=['Yes', 'No'], disabled=False)
+          
+        InternetService = st.selectbox("Internet Service", ['DSL', 'Fiber optic', 'No'])
+        if InternetService == 'No':
+          OnlineSecurity = st.selectbox("Online Security", options=['Yes', 'No'], disabled=True)
+          OnlineSecurity = 'No internet service'
+          OnlineBackup = st.selectbox("Online Backup", options=['Yes', 'No'], disabled=True)
+          OnlineBackup = 'No internet service'
+          DeviceProtection = st.selectbox("Device Protection", options=['Yes', 'No'], disabled=True)
+          DeviceProtection = 'No internet service'
+          TechSupport = st.selectbox("Tech Support", options=['Yes', 'No'], disabled=True)
+          TechSupport = 'No internet service'
+          StreamingTV = st.selectbox("Streaming TV", options=['Yes', 'No'], disabled=True)
+          StreamingTV = 'No internet service'
+          StreamingMovies = st.selectbox("Streaming Movies", options=['Yes', 'No'], disabled=True)
+          StreamingMovies = 'No internet service'
+        else:
+          OnlineSecurity = st.selectbox("Online Security", options=['Yes', 'No'], disabled=False)
+          OnlineBackup = st.selectbox("Online Backup", options=['Yes', 'No'], disabled=False)
+          DeviceProtection = st.selectbox("Device Protection", options=['Yes', 'No'], disabled=False)
+          TechSupport = st.selectbox("Tech Support", options=['Yes', 'No'], disabled=False)
+          StreamingTV = st.selectbox("Streaming TV", options=['Yes', 'No'], disabled=False)
+          StreamingMovies = st.selectbox("Streaming Movies", options=['Yes', 'No'], disabled=False)
 
+        Contract = st.selectbox("Contract", ['Month-to-month', 'One year', 'Two year'])
+        PaperlessBilling = st.selectbox("Paperless Billing", ['Yes', 'No'])
+        PaymentMethod = st.selectbox("Payment Method", ['Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)'])
+        MonthlyCharges = st.number_input("Monthly Charges")   
+        TotalCharges = st.number_input("Total Charges")
+      
         submitted = st.form_submit_button("Submit")
 
     if submitted:
         with st.expander("Your Selected Options"):
             result = {
-                "ID SPK": id_spk,
-                "Tipe Order": tipe_order,
-                "ID Konsumen": id_cust,
-                "Tgl Masuk Naskah": tgl_naskah,
-                "Tgl Mulai Proses": tgl_proses,
-                "Judul Naskah": judul_naskah,
-                "Tanggal Deal": tgl_deal,
-                "Sales Order": sales_order,
-                "Kuantitas": kuantitas
+                "customerID": customerID,
+                "gender": gender,
+                "SeniorCitizen": SeniorCitizen,
+                "Partner": Partner,
+                "Dependents": Dependents,
+                "tenure": tenure,
+                "PhoneService": PhoneService,
+                "MultipleLines": MultipleLines,
+                "InternetService": InternetService,
+                "OnlineSecurity": OnlineSecurity,
+                "OnlineBackup": OnlineBackup,
+                "DeviceProtection": DeviceProtection,
+                "TechSupport": TechSupport,
+                "StreamingTV": StreamingTV,
+                "StreamingMovies": StreamingMovies,
+                "Contract": Contract,
+                "PaperlessBilling": PaperlessBilling,
+                "PaymentMethod": PaymentMethod,
+                "MonthlyCharges": MonthlyCharges,
+                "TotalCharges": TotalCharges
             }
 
         # st.write(result)
 
-        feature = pd.read_csv(os.path.join('feature.csv'))
-        df1 = pd.read_csv(os.path.join('df1.csv'))
-        data_baru = result
-        df_baru = pd.DataFrame(data_baru, index=[0])
+        feature = pd.read_csv(os.path.join('feature_churn.csv'))
+        df1 = pd.read_csv(os.path.join('df1_churn.csv'))
+        df_baru = pd.DataFrame(result, index=[0])
         st.write("Your Selected Options :")
         st.table(df_baru)
-        df_baru['Tgl Masuk Naskah'] = pd.to_datetime(df_baru['Tgl Masuk Naskah']).dt.strftime('%Y-%m-%d')
-        df_baru['Tgl Mulai Proses'] = pd.to_datetime(df_baru['Tgl Mulai Proses']).dt.strftime('%Y-%m-%d')
-        df_baru['Tanggal Deal'] = pd.to_datetime(df_baru['Tanggal Deal']).dt.strftime('%Y-%m-%d')
-        df_lama = df1[df1['ID Konsumen'] == df_baru.loc[0, 'ID Konsumen']]
-        df_id = pd.concat([df_lama, df_baru], ignore_index=True).sort_values(by=['Tanggal Deal']).reset_index(drop=True)
+        df_baru.drop(columns='customerID', inplace=True)
+      
+        # data cleaning
+        addService = ['MultipleLines', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies']
+        for service in addService:
+          df[service] = df[service].apply(lambda x: 'Yes' if x=='Yes' else 'No')
 
-        # feature engineering
-        idx = df_id.loc[df_id['ID SPK'] == df_baru.loc[0, 'ID SPK']].index
+        # label encoding
+        for col in df1.columns:
+          if df1[col].dtype == 'object':
+              if len(list(df1[col].unique())) <= 2:
+                le = LabelEncoder()
+                df1[col] = le.fit_transform(df1[col])
+                df_baru[col] = le.transform(df_baru[col])
 
-        if str(df_baru.loc[0, 'Tipe Order']) == 'order':
-            df_baru.loc[0, 'First Order'] = 1
-        else:
-            df_baru.loc[0, 'First Order'] = 0
-
-        df_baru.loc[0, 'Reorder Count'] = ((df_id['Tanggal Deal'] < df_baru.loc[0, 'Tanggal Deal']) & (df_id['Tipe Order'] == 'reorder')).sum()
-        df_baru.loc[0, 'Cetak Ulang Count'] = ((df_id['Tanggal Deal'] < df_baru.loc[0, 'Tanggal Deal']) & (df_id['Tipe Order'] == 'cetak ulang')).sum()
-        df_baru.loc[0, 'Order Ebook Count'] = ((df_id['Tanggal Deal'] < df_baru.loc[0, 'Tanggal Deal']) & (df_id['Tipe Order'] == 'order ebook')).sum()
-        df_baru.loc[0, 'Cancel Count'] = ((df_id['Tanggal Deal'] < df_baru.loc[0, 'Tanggal Deal']) & (df_id['Tipe Order'] == 'cancel')).sum()
-        df_baru.loc[0, 'Retur Count'] = ((df_id['Tanggal Deal'] < df_baru.loc[0, 'Tanggal Deal']) & (df_id['Tipe Order'] == 'retur')).sum()
-
-        df_baru.loc[0, 'Day Diff Proses to Deal'] = (pd.to_datetime(df_baru.loc[0, 'Tanggal Deal']) - pd.to_datetime(df_baru.loc[0, 'Tgl Mulai Proses'])).days
-
-        if idx == 0:
-            df_baru.loc[0, 'Day Diff to Prev Trx'] = -1
-        else:
-            df_baru.loc[0, 'Day Diff to Prev Trx'] = (pd.to_datetime(df_baru.loc[0, 'Tanggal Deal']) - pd.to_datetime(df_id.loc[idx-1, 'Tanggal Deal']).max()).days
-
-        if (str(df_baru.loc[0, 'Tipe Order']) == 'cancel' or 'retur') and (idx == 0):
-            df_baru.loc[0, 'Day Diff to First Order'] = -1
-        else:
-            df_baru.loc[0, 'Day Diff to First Order'] = (pd.to_datetime(df_baru.loc[0, 'Tanggal Deal']) - pd.to_datetime(df_id.loc[df_id['First Order'] == 1, 'Tanggal Deal']).min()).days
-
-        df_baru.loc[0, 'Day Diff to Last Trx'] = (pd.to_datetime(df_baru.loc[0, 'Tanggal Deal']) - pd.to_datetime(df_id.loc[df_id.index[-1], 'Tanggal Deal'])).days
-
-        df_baru.drop(columns=['ID SPK', 'Tgl Masuk Naskah', 'Tgl Mulai Proses',
-                     'ID Konsumen', 'Tanggal Deal', 'Judul Naskah'], inplace=True)
-
-        # encoding data kategorikal
+        # one-hot encoding
         df_baru = pd.get_dummies(df_baru)
-        for kolom in feature.columns:
-            if kolom not in df_baru.columns:
-                df_baru[kolom] = 0
-
+        for col in feature.columns:
+            if col not in df_baru.columns:
+                df_baru[col] = 0
+      
+        # feature engineering
+        services = ['PhoneService', 'MultipleLines', 'InternetService_DSL', 'InternetService_Fiber optic', 'InternetService_No',
+            'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies']
+        df_baru['Sum_of_Services'] = df_baru[services].sum(axis=1)
+        
         df_baru = df_baru[feature.columns]  # match column
 
         # scaling
-        scaler = RobustScaler()
+        scaler = StandardScaler()
         df_scaled = scaler.fit_transform(feature)
         df_baru_scaled = scaler.transform(df_baru)
 
@@ -128,17 +158,17 @@ def run_ml_app():
         st.subheader('Prediction Result')
         single_array = np.array(df_baru_scaled).reshape(1, -1)
 
-        model = load_model("model_lgbm.pkl")
+        model = load_model("model_lr.pkl")
 
         prediction = model.predict(single_array)
 
         if prediction == 0:
             st.info("""
                 Hasil Prediksi Churn : 
-                Konsumen tidak churn
+                Customer tidak churn
                 """)
         elif prediction == 1:
             st.info("""
                 Hasil Prediksi Churn : 
-                Konsumen churn
+                Customer churn
                 """)
