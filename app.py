2a9a076b7377d67522b8d816ed3deb081bb73fd0
Inas Fathinah S <61815171+inasfthnh@users.noreply.github.com>
2024-05-30 16:41:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def main():
 
     menu = ['Home', 'Machine Learning']
     with st.sidebar:
-	stc.html("""
+        stc.html("""
                     <style>
                         .circle-image {
                             width: 150px;
@@ -51,9 +51,9 @@ def main():
                         <img src="https://www.shutterstock.com/image-vector/logo-inspiration-telecom-business-260nw-1889984209.jpg" />                 
                     </div>
                   """)
-	st.subheader('TELECOM COMPANY')
-	st.write("---")
-	choice = st.selectbox("Menu", menu)
+        st.subheader('TELECOM COMPANY')
+        st.write("---")
+        choice = st.selectbox("Menu", menu)
 
     if choice == 'Home':
         st.subheader("Welcome to Homepage")
