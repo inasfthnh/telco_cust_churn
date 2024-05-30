710a09e94253b3bb849ca26c74e12c0cfe1bb64a
Inas Fathinah S <61815171+inasfthnh@users.noreply.github.com>
2024-05-30 16:33:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def main():
                     </div>
                     """
                  )
-	st.subheader('')    
+	st.subheader(' b')    
         st.subheader('TELECOM COMPANY')
         st.write("---")
         choice = st.selectbox("Menu", menu)
